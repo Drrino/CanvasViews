e6a6eabdd292948517177a2c6cf4d149b66e619b
drrino <q592926752@gmail.com>
2016-05-13 13:53:08 +0800

    图片文字

--- app/src/main/java/drrino/canvasviews/weight/PictureViews.java
+++ app/src/main/java/drrino/canvasviews/weight/PictureViews.java
@@ -16,20 +16,31 @@ import android.view.View;
 public class PictureViews extends View {
   //1.创建Picture
   private Picture mPicture = new Picture();
+  private Paint textPaint = new Paint();
 
   public PictureViews(Context context) {
     super(context);
+    initPaint();
     recording();
   }
 
   public PictureViews(Context context, AttributeSet attrs) {
     super(context, attrs);
+    initPaint();
     recording();
   }
 
   public PictureViews(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+    initPaint();
     recording();
+  }
+
+  //初始化画笔
+  private void initPaint() {
+    textPaint.setColor(Color.BLACK);
+    textPaint.setStyle(Paint.Style.FILL);
+    textPaint.setTextSize(50);
   }
 
   //2.录制内容方法
@@ -47,7 +58,38 @@ public class PictureViews extends View {
 
   @Override protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
-    CanvasPicture(canvas);
+    //CanvasPicture(canvas);
+
+    //DrawText(canvas, textPaint);
+
+    DrawPosText(canvas, textPaint);
+  }
+
+  /**
+   * 不推荐使用
+   *
+   * 1.必须指定所有字符位置，否则直接crash掉
+   * 2.性能不佳，在大量使用的时候可能导致卡顿
+   * 3.不支持emoji等特殊字符，不支持字形组合与分解
+   */
+  private void DrawPosText(Canvas canvas, Paint textPaint) {
+    String str = "SLOOP";
+    canvas.drawPosText(str, new float[] {
+        100, 100,    // 第一个字符位置
+        200, 200,    // 第二个字符位置
+        300, 300,    // ...
+        400, 400, 500, 500
+    }, textPaint);
+  }
+
+  private void DrawText(Canvas canvas, Paint textPaint) {
+    String str = "ABCDEFGHIJK";
+    // 字符数组(要绘制的内容)
+    char[] chars = "ABCDEFGHIJK".toCharArray();
+
+    //canvas.drawText(str, 200, 500, textPaint);
+    //canvas.drawText(str,1,3,200,500,textPaint);//截取
+    canvas.drawText(chars, 1, 3, 200, 500, textPaint);
   }
 
   private void CanvasPicture(Canvas canvas) {
@@ -62,7 +104,7 @@ public class PictureViews extends View {
     PictureDrawable drawable = new PictureDrawable(mPicture);
     // 设置绘制区域 -- 注意此处所绘制的实际内容不会缩放
     //setBounds是设置在画布上的绘制区域，并非根据该区域进行缩放，也不是剪裁Picture，每次都从Picture的左上角开始绘制。
-    drawable.setBounds(0,0,250,mPicture.getHeight());
+    drawable.setBounds(0, 0, 250, mPicture.getHeight());
     drawable.draw(canvas);
   }
 }
